d59dd158a93bc7df7da05b63fa55773a376fafaa
deepkothadiya13 <deepkothadiya@gmail.com>
2024-09-12 01:02:30 +0530

    opening sequence changes,in product card changes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 
 tl.to(".text", { y: "0%", duration: 2, stagger: 0.5 });
-tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
+tl.to(".slider", { y: "-100%", duration: 5, delay: 0.5 });
 tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
 tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
 tl.fromTo(".big-text", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
@@ -15,3 +15,29 @@ window.addEventListener("scroll", function () {
     header.classList.remove("scrolled");
   }
 });
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function updateVideoSource() {
+  const video = document.getElementById('responsive-video');
+  const source = document.getElementById('video-source');
+  const width = window.innerWidth;
+
+  if (width <= 768) {
+    source.src = 'abc2.mp4';
+  } else {
+    source.src = 'abc.mp4';
+  }
+
+  video.load(); // Reload the video with the new source
+}
+
+window.addEventListener('resize', updateVideoSource);
+window.addEventListener('load', updateVideoSource);
